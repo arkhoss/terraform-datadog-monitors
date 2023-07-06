401121a45e4b8dc34306ea3ac47b380efc89ef43
David Caballero <d@dcaballero.net>
2023-07-06 15:18:54 +0000

    rds monitors module

--- modules/aws/rds/main.tf
+++ modules/aws/rds/main.tf
@@ -6,7 +6,7 @@ resource "datadog_monitor" "rds_max_connections" {
   message            = "Max Connections on {{application}} : {{value}}  Notify: ${var.notification_channel}"
   escalation_message = "Escalation message ${var.notification_channel}"
 
-  query = "avg(last_5m):postgresql.max_connections{cluster-name:${var.aws_rds_cluster_name}}  >= 600 "
+  query = "avg(last_10m):avg:aws.rds.database_connections{dbclusteridentifier:${var.aws_rds_cluster_name}} by {environment,application} >= 600"
 
   monitor_thresholds {
     warning           = 500
@@ -14,7 +14,6 @@ resource "datadog_monitor" "rds_max_connections" {
     critical          = 600
     critical_recovery = 500
   }
-
 
   evaluation_delay     = 130
   notify_no_data       = false
@@ -35,7 +34,7 @@ resource "datadog_monitor" "rds_deadlocks" {
   message            = "Deadlocks detected in {{application}} : {{value}}  Notify: ${var.aws_rds_cluster_name}"
   escalation_message = "Escalation message ${var.aws_rds_cluster_name}"
 
-  query = "avg(last_5m):postgresql.deadlocks{cluster-name:${var.aws_rds_cluster_name}} by {application}  >= 0.5 "
+  query = "avg(last_10m):avg:aws.rds.deadlocks{dbclusteridentifier:${var.aws_rds_cluster_name}} by {environment,application}.as_count() >= 0.5"
 
   monitor_thresholds {
     warning           = 0.3
@@ -56,3 +55,32 @@ resource "datadog_monitor" "rds_deadlocks" {
 
   tags = ["cluster:${var.aws_rds_cluster_name}", "environment:${var.environment}"]
 }
+
+resource "datadog_monitor" "rds_deadlocks" {
+  name               = "${var.aws_account_name} Deadlocks detected "
+  type               = "metric alert"
+  message            = "Deadlocks detected in {{application}} : {{value}}  Notify: ${var.aws_rds_cluster_name}"
+  escalation_message = "Escalation message ${var.aws_rds_cluster_name}"
+
+  query = "avg(last_10m):avg:aws.rds.deadlocks{dbclusteridentifier:${var.aws_rds_cluster_name}} by {environment,application}.as_count() >= 0.5"
+
+  monitor_thresholds {
+    warning           = 0.3
+    warning_recovery  = 0.2
+    critical          = 0.5
+    critical_recovery = 0.4
+  }
+
+  new_group_delay      = 300
+  evaluation_delay     = 130
+  notify_no_data       = false
+  renotify_interval    = 10
+  renotify_occurrences = 3
+
+  notify_audit = false
+  timeout_h    = 24
+  include_tags = true
+
+  tags = ["cluster:${var.aws_rds_cluster_name}", "environment:${var.environment}"]
+}
+
